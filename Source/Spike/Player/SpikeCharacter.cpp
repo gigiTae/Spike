5ba49aa843fc271277112ffb2e6f8b693f653b1d
이기태 <107969311+gigiTae@users.noreply.github.com>
2025-03-21 06:46:16 +0900

    Controller 복습

--- Source/Spike/Player/SpikeCharacter.cpp
+++ Source/Spike/Player/SpikeCharacter.cpp
@@ -1,29 +1,44 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Player/SpikeCharacter.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+#include "InputAction.h"
 
 // Sets default values
 ASpikeCharacter::ASpikeCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
 	// Network
 	bReplicates = true;
 }
 
+
 // Called when the game starts or when spawned
 void ASpikeCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	APlayerController* PlayerController = Cast<APlayerController>(GetController());
+	if (PlayerController)
+	{
+		ULocalPlayer* LocalPlayer = PlayerController->GetLocalPlayer();
+		UEnhancedInputLocalPlayerSubsystem* InputSubsystem = LocalPlayer ?
+			LocalPlayer->GetSubsystem< UEnhancedInputLocalPlayerSubsystem>() : nullptr;
+
+		if (InputSubsystem)
+		{
+			InputSubsystem->AddMappingContext(InputMappingContext, 0);
+		}
+	}
 }
 
 // Called every frame
 void ASpikeCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
@@ -31,6 +46,32 @@ void ASpikeCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComp
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	UEnhancedInputComponent* EnhancedInput = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+
+	if (EnhancedInput)
+	{
+		EnhancedInput->BindAction(JumpAction, ETriggerEvent::Triggered, this, ACharacter::Jump);
+		EnhancedInput->BindAction(JumpAction, ETriggerEvent::Completed, this, ACharacter::StopJumping);
+		EnhancedInput->BindAction(MoveAction, ETriggerEvent::Triggered, this, ASpikeCharacter::Move);
+	}
+}
+
+void ASpikeCharacter::Move(const FInputActionValue& Value)
+{
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+
+	if (Controller)
+	{
+		const FRotator Rotation = Controller->GetControlRotation();
+
+
+		const FVector Forward = GetActorForwardVector();
+		const FVector Right = GetActorRightVector();
+
+		AddMovementInput(Forward, MovementVector.X);
+		AddMovementInput(Right, MovementVector.Y);
+	}
 
 }
+
 
--- Source/Spike/Player/SpikeCharacter.h
+++ Source/Spike/Player/SpikeCharacter.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "InputAction.h"
 #include "SpikeCharacter.generated.h"
 
 UCLASS()
@@ -19,11 +20,25 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// Input Section
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<class UInputMappingContext> InputMappingContext;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputAction> JumpAction;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputAction> RotateAction;
+
+private:
+	void Move(const FInputActionValue& Value);
 
 };
